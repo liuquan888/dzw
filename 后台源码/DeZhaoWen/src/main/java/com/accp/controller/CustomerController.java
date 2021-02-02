5348901e523bf58d5b0b20404eeede4841bd0482
mawei888 <3430463433@qq.com>
2021-02-02 14:19:32 +0800

    2-2马威

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/CustomerController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package com.accp.controller;
 
 
+import com.accp.domain.Customer;
+import com.accp.service.impl.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    CustomerServiceImpl Service;
 
+    @RequestMapping("/find")
+    @ResponseBody
+    public List<Customer> find(){
+        List<Customer> list=Service.list();
+        return list;
+    }
 }
 
