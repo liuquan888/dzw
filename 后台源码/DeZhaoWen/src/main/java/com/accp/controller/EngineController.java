175f3c90befb3574ffbfbfffe56445d250486b2b
nahong888 <2736128707@qq.com>
2021-02-03 10:54:36 +0800

    s2.3

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/EngineController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/EngineController.java
@@ -2,9 +2,11 @@ package com.accp.controller;
 
 
 import com.accp.domain.Engine;
+import com.accp.mapper.EngineMapper;
 import com.accp.service.impl.EngineServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +28,17 @@ public class EngineController {
     @Autowired
     EngineServiceImpl engineService;
 
+    //查看所有发动机品牌
     @GetMapping("/findall")
     public List<Engine> findall(){
-        System.out.print("s");
         return  engineService.list();
     }
+
+    //添加发动机品牌
+    @PostMapping("/add")
+    public boolean add(Engine engine){
+        return engineService.save(engine);
+    }
+
 }
 
