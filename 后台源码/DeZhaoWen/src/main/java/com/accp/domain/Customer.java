bb8500f073f07fabf01fb4b64e4ff64f3ea3bb83
mawei888 <3430463433@qq.com>
2021-02-04 09:41:47 +0800

    2-04马威

--- 后台源码/DeZhaoWen/src/main/java/com/accp/domain/Customer.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/domain/Customer.java
@@ -1,5 +1,7 @@
 package com.accp.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
@@ -24,6 +26,7 @@ public class Customer extends Model<Customer> {
 
     private static final long serialVersionUID=1L;
 
+    @TableId(value = "customernum")
     @ApiModelProperty(value = "客户编码")
     private String customernum;
 
