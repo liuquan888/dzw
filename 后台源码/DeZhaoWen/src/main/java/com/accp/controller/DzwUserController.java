bb2ea62cea57ad86672107932e5b75d75ec827a2
liuquan888 <384089312@qq.com>
2021-02-08 11:36:02 +0800

    更新

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/DzwUserController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/DzwUserController.java
@@ -3,10 +3,23 @@ package com.accp.controller;
 
 import com.accp.domain.DzwUser;
 import com.accp.service.impl.DzwUserServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.security.Principal;
+import java.util.*;
 
 /**
  * <p>
@@ -18,14 +31,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/dzwUser")
-public class DzwUserController {
+public class DzwUserController extends HttpServlet{
     @Autowired
-    DzwUserServiceImpl user;
+    DzwUserServiceImpl duser;
 
     @RequestMapping("/find")
     public String find(DzwUser user){
-        System.out.println(user.getUserName());
-        return "";
+        QueryWrapper<DzwUser> query=new QueryWrapper<>();
+        query.lambda().eq(DzwUser::getUserPwd,user.getUserPwd()).eq(DzwUser::getUserLogin,user.getUserLogin());
+        List<DzwUser> li=duser.list(query);
+        if(li.size()>0){
+            System.out.println(1);
+            return "登录成功";
+        }else{
+            return "登陆失败";
+        }
+
     }
 }
 
