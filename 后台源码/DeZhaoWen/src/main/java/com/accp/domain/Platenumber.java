236656cbea263cb9ac7a814c3295aca37a6210a7
mawei888 <3430463433@qq.com>
2021-02-15 10:46:38 +0800

    2-15马威

--- 后台源码/DeZhaoWen/src/main/java/com/accp/domain/Platenumber.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/domain/Platenumber.java
@@ -1,5 +1,6 @@
 package com.accp.domain;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
@@ -24,6 +25,7 @@ public class Platenumber extends Model<Platenumber> {
 
     private static final long serialVersionUID=1L;
 
+    @TableId(value = "plateid")
     @ApiModelProperty(value = "车牌id")
     private Integer plateid;
 
