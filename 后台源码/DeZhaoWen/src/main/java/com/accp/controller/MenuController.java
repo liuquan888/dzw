1883041942fb0a50b29a53d376f169dc72dde7f3
2854943854 <i2stay1941@163.com>
2021-02-21 22:26:02 +0800

    111

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/MenuController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/MenuController.java
@@ -1,9 +1,16 @@
 package com.accp.controller;
 
 
+import com.accp.domain.Menu;
+import com.accp.service.impl.MenuServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/menu")
 public class MenuController {
+    @Autowired
+    MenuServiceImpl service;
 
+    @RequestMapping("/findAll")
+    public List<Menu> findAll(){
+        List<Menu> list=service.list();
+        for (Menu m:list){
+            if(m.getParentid()!=0){
+                List<Menu> list2=new ArrayList<Menu>();
+                for (Menu m2:list){
+                    if(m2.getId()==m.getParentid()){
+                        list2.add(m);
+                        m2.setChildrens(list2);
+                    }
+                }
+            }
+        }
+        return list;
+    }
 }
 
