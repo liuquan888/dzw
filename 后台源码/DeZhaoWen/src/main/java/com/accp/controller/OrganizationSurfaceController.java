e1affea098e918599fb9d46dd3ec24c5636f1334
2064239692 <2064239692@qq.com>
2021-02-22 16:10:06 +0800

    张毅丰

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/OrganizationSurfaceController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/OrganizationSurfaceController.java
@@ -1,9 +1,14 @@
 package com.accp.controller;
 
 
+import com.accp.domain.OrganizationSurface;
+import com.accp.service.impl.OrganizationSurfaceServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/organizationSurface")
 public class OrganizationSurfaceController {
 
-}
+    @Autowired
+    OrganizationSurfaceServiceImpl o;
 
+    @RequestMapping("/find")
+    public List<OrganizationSurface> findByAll(){
+        return o.list();
+    }
+}
