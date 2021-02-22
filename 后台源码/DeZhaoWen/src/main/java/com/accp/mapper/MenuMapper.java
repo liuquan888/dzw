19cbc8aa7b07f0719506c8a134223d6322f9bc0e
2854943854 <i2stay1941@163.com>
2021-02-22 16:57:05 +0800

    11111111

--- 后台源码/DeZhaoWen/src/main/java/com/accp/mapper/MenuMapper.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/mapper/MenuMapper.java
@@ -3,6 +3,8 @@ package com.accp.mapper;
 import com.accp.domain.Menu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  * 树形菜单 Mapper 接口
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2021-02-22
  */
 public interface MenuMapper extends BaseMapper<Menu> {
-
+    List<Menu> findByParentId(Integer id);
 }
