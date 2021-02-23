057153e4486d1f6495727fea8256e912422e4add
zenglianjun666 <1436605570@qq.com>
2021-02-23 09:45:10 +0800

    增联军

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/WeixlisController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/WeixlisController.java
@@ -1,9 +1,18 @@
 package com.accp.controller;
 
 
+import com.accp.domain.Businessclass;
+import com.accp.domain.Weixlis;
+import com.accp.service.impl.BusinessclassServiceImpl;
+import com.accp.service.impl.WeixlisServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +25,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/weixlis")
 public class WeixlisController {
+
+    @Autowired
+    WeixlisServiceImpl weix;
+
+    @Autowired
+    BusinessclassServiceImpl bussine;
+
+    @GetMapping("/findweix")
+    public List<Weixlis> carList(){
+        List<Weixlis> list=weix.list();
+        for (Weixlis we:list) {
+            QueryWrapper plqw=new QueryWrapper<Businessclass>();
+            plqw.eq("businessid",we.getYwlx());
+            Businessclass p=(Businessclass) bussine.list(plqw).get(0);
+            we.setBusinessclass(p);
+        }
+        return list;
+    }
 
 }
 
