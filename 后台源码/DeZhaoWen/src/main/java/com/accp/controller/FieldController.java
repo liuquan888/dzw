6f472c1c81827832e398b7c948aedcf720d446de
2064239692 <2064239692@qq.com>
2021-02-24 16:06:24 +0800

    111

--- 后台源码/DeZhaoWen/src/main/java/com/accp/controller/FieldController.java
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/controller/FieldController.java
@@ -1,9 +1,14 @@
 package com.accp.controller;
 
 
+import com.accp.domain.Field;
+import com.accp.service.impl.FieldServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/field")
 public class FieldController {
+
+    @Autowired
+    FieldServiceImpl service;
+
+    @RequestMapping("/find")
+    public List<Field> find(){
+        return service.list();
+    }
 
 }
 
