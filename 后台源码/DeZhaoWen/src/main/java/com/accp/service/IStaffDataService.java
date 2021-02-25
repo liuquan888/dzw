77ee3cd4b43e3acb9387fd176891f08256953f04
liuquan888 <384089312@qq.com>
2021-02-25 21:02:11 +0800

    更新

---
+++ 后台源码/DeZhaoWen/src/main/java/com/accp/service/IStaffDataService.java
@@ -0,0 +1,16 @@
+package com.accp.service;
+
+import com.accp.domain.StaffData;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 员工资料表 服务类
+ * </p>
+ *
+ * @author quanl
+ * @since 2021-02-25
+ */
+public interface IStaffDataService extends IService<StaffData> {
+
+}
